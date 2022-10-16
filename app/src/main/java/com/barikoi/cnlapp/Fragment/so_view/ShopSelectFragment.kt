0b40c3bde60b1c38bd5eecf5ae3ecef9c5bc06af
PushpoRani <pushporani67@gmail.com>
2022-10-16 19:17:32 +0600

    added shoplist and product list design

---
+++ app/src/main/java/com/barikoi/cnlapp/Fragment/so_view/ShopSelectFragment.kt
@@ -0,0 +1,32 @@
+package com.barikoi.cnlapp.Fragment.so_view
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.barikoi.cnlapp.R
+
+class ShopSelectFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shop_select, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+}
