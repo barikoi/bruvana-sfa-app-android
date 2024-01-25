4f8515008093fc570e1e270427816edb22e2e1b4
Miraz <mmrrahman508@gmail.com>
2024-01-25 17:01:15 +0600

    View extension added

---
+++ app/src/staging/java/com/barikoi/cnlapp/utils/extension/View.kt
@@ -0,0 +1,16 @@
+package com.barikoi.cnlapp.utils.extension
+
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+
+fun View.rotateViewAnimation(fromDegrees: Float, toDegrees: Float) {
+    val an: Animation = RotateAnimation(
+        fromDegrees, toDegrees, (this.width / 2).toFloat(),
+        (this.height / 2).toFloat()
+    )
+    an.duration = 500
+    an.fillAfter = true
+    an.repeatMode = Animation.RESTART
+    this.startAnimation(an)
+}
