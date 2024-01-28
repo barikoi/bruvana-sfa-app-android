ecb388bcaf702fc0ee99faba8dd7fca5bb8fcd44
Miraz <mmrrahman508@gmail.com>
2024-01-28 11:00:27 +0600

    view and date ext added

---
+++ app/src/main/java/com/barikoi/cnlapp/utils/extension/Date.kt
@@ -0,0 +1,10 @@
+package com.barikoi.cnlapp.utils.extension
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Date.formatDate(): String {
+    val df = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+    return df.format(this)
+}
