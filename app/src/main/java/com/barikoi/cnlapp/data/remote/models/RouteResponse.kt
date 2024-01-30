522a4e6613f789ee7ba2ad5e0901b0342feecf5a
Miraz <mmrrahman508@gmail.com>
2024-01-30 18:24:01 +0600

    route response added

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/RouteResponse.kt
@@ -0,0 +1,34 @@
+package com.barikoi.cnlapp.data.remote.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RouteResponse(
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("routes")
+    val routes: List<Route>,
+    @SerializedName("status_code")
+    val statusCode: Int
+)
+
+data class Route(
+    @SerializedName("area_id")
+    val areaId: Int,
+    @SerializedName("area_name")
+    val areaName: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("is_active")
+    val isActive: Int,
+    @SerializedName("route_code")
+    val routeCode: String,
+    @SerializedName("route_name")
+    val routeName: String,
+    @SerializedName("territory_id")
+    val territoryId: Int,
+    @SerializedName("territory_name")
+    val territoryName: String,
+    @SerializedName("user_id")
+    val userId: Int
+)
