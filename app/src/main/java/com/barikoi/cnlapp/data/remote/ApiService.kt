9c68881f75c8b387d1ec761d68f657cd7a7df3de
Miraz <mmrrahman508@gmail.com>
2024-01-30 18:24:40 +0600

    api service interface added

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/ApiService.kt
@@ -0,0 +1,25 @@
+package com.barikoi.cnlapp.data.remote
+
+import com.barikoi.cnlapp.data.remote.models.OutletsResponse
+import com.barikoi.cnlapp.data.remote.models.RouteResponse
+import com.barikoi.cnlapp.data.remote.models.SoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("api/v1/routes")
+    suspend fun getRoute(@Query("user_id") userId: String): Response<RouteResponse>
+
+    @GET("api/v1/get-so")
+    suspend fun getSoList(): Response<SoResponse>
+
+
+    @GET("api/v1/outlets")
+    suspend fun getOutlets(
+        @Query("route_id") routeId: String,
+        @Query("verified_outlets") isVerify: String,
+        @Query("outlet_category") outletCategory: String
+    ): Response<OutletsResponse>
+}
