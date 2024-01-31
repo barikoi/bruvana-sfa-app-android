8274e1a614d911deea545fdae7dd22b0de6062c0
Miraz <mmrrahman508@gmail.com>
2024-01-31 12:03:09 +0600

    toast etx added

--- app/src/main/java/com/barikoi/cnlapp/utils/extension/View.kt
+++ app/src/main/java/com/barikoi/cnlapp/utils/extension/View.kt
@@ -1,8 +1,11 @@
 package com.barikoi.cnlapp.utils.extension
 
+import android.app.Activity
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 fun View.rotateViewAnimation(fromDegrees: Float, toDegrees: Float) {
     val an: Animation = RotateAnimation(
@@ -13,4 +16,12 @@ fun View.rotateViewAnimation(fromDegrees: Float, toDegrees: Float) {
     an.fillAfter = true
     an.repeatMode = Animation.RESTART
     this.startAnimation(an)
+}
+
+fun Fragment.toast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
