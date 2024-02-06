9c45fcd943b4c8e91505170326dc6c8ee915c2a1
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-06 16:59:27 +0600

    rafactor acording to the staging

--- app/src/production/java/com/barikoi/cnlapp/Activity/CreateShopActivity.kt
+++ app/src/production/java/com/barikoi/cnlapp/Activity/CreateShopActivity.kt
@@ -32,9 +32,9 @@ import com.android.volley.toolbox.StringRequest
 import com.barikoi.cnlapp.Model.Shops
 import com.barikoi.cnlapp.Order_Create.Callback.DialogListener
 import com.barikoi.cnlapp.R
-import com.barikoi.cnlapp.Utils.*
-import com.barikoi.cnlapp.Utils.ApiService.ApiServiceListener
-import com.barikoi.cnlapp.Utils.ApiService.ApiServices
+import com.barikoi.cnlapp.utils.*
+import com.barikoi.cnlapp.utils.ApiService.ApiServiceListener
+import com.barikoi.cnlapp.utils.ApiService.ApiServices
 import com.barikoi.cnlapp.imagecapture.RoomDb.ImageDatabase
 import com.barikoi.cnlapp.imagecapture.RoomDb.Images
 import com.barikoi.cnlapp.imagecapture.Utils.ApiCall
@@ -193,7 +193,7 @@ class CreateShopActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsL
             val btnSubmit = dialog.findViewById<AppCompatButton>(R.id.btnSubmit)
             val btnClose = dialog.findViewById<ImageButton>(R.id.btnClose)
 
-            mapView = dialog.findViewById<MapView>(R.id.mapview)
+            mapView = dialog.findViewById<MapView>(R.id.map_view)
             mapView!!.onCreate(savedInstanceState)
             mapView!!.getMapAsync(this)
             mapView!!.onStart()
