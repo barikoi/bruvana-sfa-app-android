c62e02e6230f9f4136993ccba646195ca6729faa
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-15 12:04:50 +0600

    trace login api implementation added

--- app/src/main/java/com/barikoi/cnlapp/data/remote/repository/SocketRepositoryImpl.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/SocketRepositoryImpl.kt
@@ -6,15 +6,19 @@ import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
 import com.barikoi.cnlapp.data.remote.api.SocketApiService
 import com.barikoi.cnlapp.data.remote.models.SocketGroupResponse
 import com.barikoi.cnlapp.data.remote.models.SocketUserResponse
+import com.barikoi.cnlapp.data.remote.models.trace.TraceLoginResponse
+import com.barikoi.cnlapp.data.remote.models.trace.request.TraceLognRequest
 import com.barikoi.cnlapp.utils.AppLogger
 import io.sentry.Sentry
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.http.Body
 import java.net.UnknownHostException
 import javax.inject.Inject
 
 interface SocketRepository {
 
+    fun traceLogin(loginRequest: TraceLognRequest): Flow<ApiState<TraceLoginResponse>>
     fun getAllGroup(): Flow<ApiState<SocketGroupResponse>>
     fun getAllUsersByGroupID(groupId: String): Flow<ApiState<SocketUserResponse>>
 
@@ -22,6 +26,37 @@ interface SocketRepository {
 
 class SocketRepositoryImpl @Inject constructor(private val socketApiService: SocketApiService) :
     SocketRepository {
+    override fun traceLogin(loginRequest: TraceLognRequest): Flow<ApiState<TraceLoginResponse>> {
+        return flow {
+            try {
+                val response =
+                    socketApiService.traceLogin(loginRequest)
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
     override fun getAllGroup(): Flow<ApiState<SocketGroupResponse>> {
         return flow {
             try {
