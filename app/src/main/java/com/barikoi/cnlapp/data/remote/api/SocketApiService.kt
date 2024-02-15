d6f6c22b187af03718b76dac19bdcefaee0df314
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-15 12:06:22 +0600

    trace login api interface added

--- app/src/main/java/com/barikoi/cnlapp/data/remote/api/SocketApiService.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/api/SocketApiService.kt
@@ -2,11 +2,19 @@ package com.barikoi.cnlapp.data.remote.api
 
 import com.barikoi.cnlapp.data.remote.models.SocketGroupResponse
 import com.barikoi.cnlapp.data.remote.models.SocketUserResponse
+import com.barikoi.cnlapp.data.remote.models.trace.TraceLoginResponse
+import com.barikoi.cnlapp.data.remote.models.trace.request.TraceLognRequest
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface SocketApiService {
+
+    @POST("auth/login")
+    suspend fun traceLogin(@Body body: TraceLognRequest): Response<TraceLoginResponse>
+
     @GET("company/groups")
     suspend fun getSocketGroups(
     ): Response<SocketGroupResponse>
