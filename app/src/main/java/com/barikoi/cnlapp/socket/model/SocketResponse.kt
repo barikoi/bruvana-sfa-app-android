5bfaa0b4b40fefb0c8c041cd9d79fd7bcd38d8c5
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-15 12:07:03 +0600

    socket response model added

---
+++ app/src/main/java/com/barikoi/cnlapp/socket/model/SocketResponse.kt
@@ -0,0 +1,35 @@
+package com.barikoi.cnlapp.socket.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SocketResponse(
+    @SerializedName("accuracy")
+    val accuracy: Double,
+    @SerializedName("altitude")
+    val altitude: Double,
+    @SerializedName("bearing")
+    val bearing: Double,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("gpx_time")
+    val gpxTime: String,
+    @SerializedName("_id")
+    val id: String,
+    @SerializedName("is_offline_data")
+    val isOfflineData: Boolean,
+    @SerializedName("latitude")
+    val latitude: Double,
+    @SerializedName("location")
+    val location: Location,
+    @SerializedName("longitude")
+    val longitude: Double,
+    @SerializedName("speed")
+    val speed: Double,
+    @SerializedName("updated_at")
+    val updatedAt: String,
+    @SerializedName("user")
+    val user: String,
+    @SerializedName("__v")
+    val v: Int
+)
