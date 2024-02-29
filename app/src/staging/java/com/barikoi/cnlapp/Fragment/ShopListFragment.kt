c86bae9598f25e8113d02eeb4fab59c22483d332
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-29 11:13:20 +0600

    model changes

--- app/src/staging/java/com/barikoi/cnlapp/Fragment/ShopListFragment.kt
+++ app/src/staging/java/com/barikoi/cnlapp/Fragment/ShopListFragment.kt
@@ -274,7 +274,8 @@ class ShopListFragment : Fragment(), OnEditShopListener {
                                         route_id,
                                         route_name,
                                         "",
-                                        is_Verified,0, 0
+                                        is_Verified,0,
+                                        0,0.0f
                                     )
                                 )
                             }
