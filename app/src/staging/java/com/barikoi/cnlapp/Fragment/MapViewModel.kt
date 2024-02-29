357c7dfc019fd75e1e4be067c56b380fbc20216c
Moklesur Rahman <mmrrahman508@gmail.com>
2024-02-29 10:27:25 +0600

    socket user model changes

--- app/src/staging/java/com/barikoi/cnlapp/Fragment/MapViewModel.kt
+++ app/src/staging/java/com/barikoi/cnlapp/Fragment/MapViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.data.model.socket.SocketGroupUsersResponse
 import com.barikoi.cnlapp.data.remote.repository.RouteRepository
 import com.barikoi.cnlapp.data.remote.models.OutletsResponse
 import com.barikoi.cnlapp.data.remote.models.RouteResponse
 import com.barikoi.cnlapp.data.remote.models.SoResponse
 import com.barikoi.cnlapp.data.remote.models.SocketGroupResponse
-import com.barikoi.cnlapp.data.remote.models.SocketUserResponse
 import com.barikoi.cnlapp.data.remote.repository.SocketRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -39,8 +39,8 @@ class MapViewModel @Inject constructor(
     val socketGroupResponse: LiveData<ApiState<SocketGroupResponse>> = _socketGroupResponse
 
 
-    private val _socketUsersResponse = MutableLiveData<ApiState<SocketUserResponse>>()
-    val socketUsersResponse: LiveData<ApiState<SocketUserResponse>> = _socketUsersResponse
+    private val _socketUsersResponse = MutableLiveData<ApiState<SocketGroupUsersResponse>>()
+    val socketUsersResponse: LiveData<ApiState<SocketGroupUsersResponse>> = _socketUsersResponse
 
     fun getSocketGroups() {
         viewModelScope.launch {
