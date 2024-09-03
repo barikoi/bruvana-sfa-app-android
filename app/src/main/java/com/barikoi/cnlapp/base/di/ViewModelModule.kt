546d7f1d3acbbac9d6c1c23a3127556d144dd450
Moklesur Rahman <mmrrahman508@gmail.com>
2024-09-03 17:35:05 +0600

    provider added

--- app/src/main/java/com/barikoi/cnlapp/base/di/ViewModelModule.kt
+++ app/src/main/java/com/barikoi/cnlapp/base/di/ViewModelModule.kt
@@ -4,6 +4,8 @@ import com.barikoi.cnlapp.data.remote.api.ApiService
 import com.barikoi.cnlapp.data.remote.repository.RouteRepository
 import com.barikoi.cnlapp.data.remote.repository.RouteRepositoryImpl
 import com.barikoi.cnlapp.data.remote.api.SocketApiService
+import com.barikoi.cnlapp.data.remote.repository.ProductStockRepository
+import com.barikoi.cnlapp.data.remote.repository.ProductStockRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.SocketRepository
 import com.barikoi.cnlapp.data.remote.repository.SocketRepositoryImpl
 import dagger.Module
@@ -22,4 +24,8 @@ object ViewModelModule {
     @Provides
     fun providesSocketRepository(socketApiService: SocketApiService): SocketRepository =
         SocketRepositoryImpl(socketApiService)
+
+    @Provides
+    fun providesProductStockRepository(apiService: ApiService): ProductStockRepository =
+        ProductStockRepositoryImpl(apiService)
 }
