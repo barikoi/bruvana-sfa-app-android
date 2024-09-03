504e90710b14fd2605a2f3560384f62ef19af49a
Moklesur Rahman <mmrrahman508@gmail.com>
2024-09-03 17:31:57 +0600

    notification init added

--- app/src/main/java/com/barikoi/cnlapp/utils/CNLApp.kt
+++ app/src/main/java/com/barikoi/cnlapp/utils/CNLApp.kt
@@ -2,13 +2,19 @@ package com.barikoi.cnlapp.utils
 
 import android.app.Application
 import android.content.Context
-import android.content.res.Configuration
-import android.content.res.Resources
+import com.barikoi.barikoitrace.BarikoiTrace
+import com.barikoi.cnlapp.BuildConfig
+import com.barikoi.cnlapp.utils.extension.NotificationOpenedHandler
 import com.mapbox.mapboxsdk.Mapbox
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
 import io.sentry.android.core.SentryAndroid
 import io.sentry.android.core.SentryAndroidOptions
-import java.util.Locale
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 @HiltAndroidApp
 class CNLApp : Application() {
@@ -22,28 +28,28 @@ class CNLApp : Application() {
             options.isEnableAutoSessionTracking = true
         }
 
-        /*// Replace YOUR_ENVIRONMENT_ID with the ID of the Heap environment you wish to send data to.
-        Heap.startRecording(this, "2261727323")
-        // Call ViewAutocaptureSDK.register() to enable autocapture for supported UI elements.
-        ViewAutocaptureSDK.register()*/
+        // TODO:: NEED TO CHANGE FOR OTHERS FLAVORS
+        BarikoiTrace.initialize(this, Api.APIKEY)
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+
+//         OneSignal Initialization
+        OneSignal.initWithContext(this, BuildConfig.ONESIGNAL_APP_ID)
+        OneSignal.Notifications.addClickListener(NotificationOpenedHandler(this))
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(false)
+        }
     }
 
     companion object {
-
         lateinit var appContext: Context
-
     }
 
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        val res: Resources = base!!.resources
-
-        val locale = Locale("en")
-        Locale.setDefault(locale)
-
-        val config = Configuration()
-        config.locale = locale
-
-        res.updateConfiguration(config, res.getDisplayMetrics())
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(DefaultLocaleHelper.getInstance(newBase!!).onAttach())
     }
 }
