7606bbc78e0ba72fd9a0f394ff9b1ac84827a43a
Moklesur Rahman <mmrrahman508@gmail.com>
2024-10-27 14:37:26 +0600

    image adapter cganges

---
+++ app/src/main/java/com/barikoi/cnlapp/base/adapter/AdapterImagePickerView.kt
@@ -0,0 +1,45 @@
+package com.barikoi.cnlapp.base.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.barikoi.cnlapp.databinding.ItemImageBinding
+import com.bumptech.glide.Glide
+
+class AdapterImagePickerView(
+    private val onImageCloseClick: (Int) -> Unit
+) : Adapter<AdapterImagePickerView.ImagePickerViewHolder>() {
+
+    var images: List<String> = emptyList()
+
+    inner class ImagePickerViewHolder(val binding: ItemImageBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagePickerViewHolder {
+        return ImagePickerViewHolder(
+            ItemImageBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = images.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateImages(images: List<String>) {
+        this.images = images
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ImagePickerViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(images[position])
+            .into(holder.binding.ivPicImage)
+
+        holder.binding.ivClose.setOnClickListener {
+            onImageCloseClick(position)
+        }
+    }
+}
