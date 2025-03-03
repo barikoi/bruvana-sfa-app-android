c33dce4759888d8e598dd2f98daa7a267e1e84dd
Miraz <mmrrahman508@gmail.com>
2025-03-03 14:33:30 +0600

    feat: enhance AdapterGift to support gift image and quantity management
    The AdapterGift class has been updated to enhance functionality.
    - Changed the inner `GiftViewHolder` class to use `ItemAddGift2Binding` instead of the older binding.
    - Added support for displaying gift images with `coil3`.
    - Replaced image picker functionality with a `btnAdd` button.
    - Introduced counter management, with `incrementClickListener` and `decrementClickListener`.
    - Replaced image adding with a new logic that now manages gift quantity.
    - Added conditional visibility for `llCardCounter` and `btnAdd` based on the existence of gift images.
    - Updated `updateList` to handle a `List<Gift>` instead of older type.

--- app/src/main/java/com/barikoi/cnlapp/ui/add_gift/AdapterGift.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/add_gift/AdapterGift.kt
@@ -3,30 +3,36 @@ package com.barikoi.cnlapp.ui.add_gift
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil3.load
+import coil3.request.fallback
+import coil3.request.placeholder
+import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.base.adapter.AdapterImagePickerView
-import com.barikoi.cnlapp.databinding.ItemAddGiftBinding
+import com.barikoi.cnlapp.data.remote.models.Gift
+import com.barikoi.cnlapp.databinding.ItemAddGift2Binding
 import com.barikoi.cnlapp.utils.extension.setHapticClickListener
 
 class AdapterGift(
-    private val addImageClickListener: (Int) -> Unit,
-    private val removeImageClickListener: (Int) -> Unit,
+    private val addClickListener: (Int) -> Unit,
+    private val incrementClickListener: (Int) -> Unit,
+    private val decrementClickListener: (Int) -> Unit,
     private val removeItemClickListener: (Int) -> Unit
 ) : Adapter<AdapterGift.GiftViewHolder>() {
 
-    private var giftList: List<GiftDataModel> = emptyList()
+    private var giftList: List<Gift> = emptyList()
 
     private var adapterImage: AdapterImagePickerView = AdapterImagePickerView {
 
     }
 
-    inner class GiftViewHolder(val binding: ItemAddGiftBinding) : ViewHolder(binding.root)
+    inner class GiftViewHolder(val binding: ItemAddGift2Binding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GiftViewHolder {
         return GiftViewHolder(
-            ItemAddGiftBinding.inflate(
+            ItemAddGift2Binding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
@@ -37,31 +43,36 @@ class AdapterGift(
     override fun getItemCount(): Int = giftList.size
 
     @SuppressLint("NotifyDataSetChanged")
-    fun updateList(list: List<GiftDataModel>) {
+    fun updateList(list: List<Gift>) {
         giftList = list
         notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: GiftViewHolder, position: Int) {
-        holder.binding.tvGiftName.text = giftList[position].name
+        holder.binding.tvGiftName.text =
+            giftList[position].name
 
-        holder.binding.llImagePickerView.rvImage.adapter = adapterImage
+        holder.binding.tvCount.setText(giftList[position].qty.toString())
 
-        val layoutManager = LinearLayoutManager(
-            holder.binding.llImagePickerView.rvImage.context,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        holder.binding.llImagePickerView.rvImage.layoutManager = layoutManager
-
-        adapterImage.updateImages(giftList[position].images)
-
-        holder.binding.llImagePickerView.ivPicImage.setOnClickListener {
-            addImageClickListener(position)
+        holder.binding.ivGift.load(
+            giftList[position].image,
+        ) {
+            placeholder(R.drawable.image_place_holder) // Correct way to set placeholder
+            fallback(R.drawable.image_place_holder) // Optional: Show if URL is null
         }
 
-        holder.binding.ivClose.setHapticClickListener {
-            removeItemClickListener(position)
+        if (giftList[position].images.isNullOrEmpty()) {
+            holder.binding.llCardCounter.isVisible = false
+            holder.binding.btnAdd.isVisible = true
+        } else {
+            holder.binding.llCardCounter.isVisible = true
+            holder.binding.btnAdd.isVisible = false
         }
+
+
+        holder.binding.btnAdd.setHapticClickListener {
+            addClickListener.invoke(position)
+        }
+
     }
 }
