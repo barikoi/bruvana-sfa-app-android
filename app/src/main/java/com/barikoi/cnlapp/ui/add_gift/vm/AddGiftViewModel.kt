fe31f869d2d324d9ba2da3a6a47ae966fc575f6c
Miraz <mmrrahman508@gmail.com>
2025-03-05 11:31:34 +0600

    Refactor: Rename AddGiftViewModel and add save gift functionality
    - Renamed `AddGiftViewModel` to reside within the `vm` package for better organization.
    - Added a new LiveData `saveGiftResponse` to handle API responses for saving gifts.
    - Implemented a `saveGift` function to communicate with the repository to persist gifts.
    - Added `BaseResponse` class to manage response of save gift

--- app/src/main/java/com/barikoi/cnlapp/ui/add_gift/vm/AddGiftViewModel.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/add_gift/vm/AddGiftViewModel.kt
@@ -1,15 +1,17 @@
-package com.barikoi.cnlapp.ui.add_gift
+package com.barikoi.cnlapp.ui.add_gift.vm
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.data.remote.models.BaseResponse
 import com.barikoi.cnlapp.data.remote.models.GiftResponse
 import com.barikoi.cnlapp.data.remote.repository.AddGiftRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import okhttp3.RequestBody
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,7 +21,10 @@ class AddGiftViewModel @Inject constructor(
 
 
     private val _giftResponse = MutableLiveData<ApiState<GiftResponse>>()
-    val routeResponse: LiveData<ApiState<GiftResponse>> = _giftResponse
+    val giftResponse: LiveData<ApiState<GiftResponse>> = _giftResponse
+
+    private val _saveGiftResponse = MutableLiveData<ApiState<BaseResponse>>()
+    val saveGiftResponse: LiveData<ApiState<BaseResponse>> = _saveGiftResponse
 
     fun getGifts() {
         viewModelScope.launch {
@@ -31,5 +36,15 @@ class AddGiftViewModel @Inject constructor(
             }
         }
 
+    }
+
+    fun saveGift(body: RequestBody) {
+        viewModelScope.launch {
+            addGiftRepository.saveGifts(body).onStart {
+                _saveGiftResponse.value = ApiState.Loading()
+            }.collect {
+                _saveGiftResponse.value = it
+            }
+        }
     }
 }
