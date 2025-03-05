268c97368d695f3edcb6fe4228cbfc4923772f45
Miraz <mmrrahman508@gmail.com>
2025-03-05 11:31:01 +0600

    remove GiftDataModel file

--- app/src/main/java/com/barikoi/cnlapp/ui/add_gift/GiftDataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.barikoi.cnlapp.ui.add_gift
-
-import com.barikoi.cnlapp.data.remote.models.Gift
-
-data class GiftDataModel(
-    val id: Int,
-    val name: String,
-    val gift: List<Gift>
-)
