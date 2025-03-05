26c851e9c7743579087caa829a9a13eab7056681
Miraz <mmrrahman508@gmail.com>
2025-03-05 11:33:06 +0600

    Refactor: Rename and reorganize AdapterMainGift for clarity
    - Renamed `AdapterMainGift` to improve organization and reflect its role.
    - Moved `AdapterMainGift` to a dedicated `adapter` package.
    - Introduced a nested `AdapterGift` within `AdapterMainGift`.
    - Adjusted click listeners to include position information, enabling targeted actions.
    - Added `updateByPos` method for dynamic updates of the nested gift list.

--- app/src/main/java/com/barikoi/cnlapp/ui/add_gift/adapter/AdapterMainGift.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/add_gift/adapter/AdapterMainGift.kt
@@ -1,4 +1,4 @@
-package com.barikoi.cnlapp.ui.add_gift
+package com.barikoi.cnlapp.ui.add_gift.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
@@ -7,17 +7,20 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.barikoi.cnlapp.base.adapter.AdapterImagePickerView
+import com.barikoi.cnlapp.data.remote.models.Gift
 import com.barikoi.cnlapp.databinding.ItemGiftMainBinding
+import com.barikoi.cnlapp.data.remote.models.GiftModel
 
 class AdapterMainGift(
     private val addClickListener: (Int, Int) -> Unit,
-    private val incrementClickListener: (Int) -> Unit,
-    private val decrementClickListener: (Int) -> Unit,
-    private val removeItemClickListener: (Int) -> Unit
+    private val incrementClickListener: (Int, Int) -> Unit,
+    private val decrementClickListener: (Int, Int) -> Unit,
+    private val removeItemClickListener: (Int, Int) -> Unit
 ) : Adapter<AdapterMainGift.GiftViewHolder>() {
 
-
     private var giftList: List<GiftModel> = emptyList()
+
+    private lateinit var adapterGift: AdapterGift
 
     private var adapterImage: AdapterImagePickerView = AdapterImagePickerView {
 
@@ -43,16 +46,27 @@ class AdapterMainGift(
         notifyDataSetChanged()
     }
 
+    fun updateByPos(pos: Int, list: List<Gift>) {
+        adapterGift.updateByPos(pos, list)
+    }
+
     override fun onBindViewHolder(holder: GiftViewHolder, position: Int) {
         holder.binding.tvTitle.text = giftList[position].title
 
-        val adapterGift = AdapterGift(
+        adapterGift = AdapterGift(
             addClickListener = {
                 addClickListener(position, it)
             },
-            incrementClickListener,
-            decrementClickListener,
-            removeItemClickListener
+            incrementClickListener = {
+                incrementClickListener(position, it)
+            },
+            decrementClickListener = {
+                decrementClickListener(position, it)
+            },
+
+            removeItemClickListener = {
+                removeItemClickListener(position, it)
+            }
         )
 
 
@@ -60,6 +74,5 @@ class AdapterMainGift(
             GridLayoutManager(holder.itemView.context, 1, GridLayoutManager.HORIZONTAL, false)
         holder.binding.rcvGiftMain.adapter = adapterGift
         adapterGift.updateList(giftList[position].gifts)
-
     }
 }
