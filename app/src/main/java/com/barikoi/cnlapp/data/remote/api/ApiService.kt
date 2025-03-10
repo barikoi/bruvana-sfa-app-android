92535d1bf58002f79cb4277d7469e9f5c553cac5
Miraz <mmrrahman508@gmail.com>
2025-03-10 14:03:09 +0600

    api changes for dhlist and today summary

--- app/src/main/java/com/barikoi/cnlapp/data/remote/api/ApiService.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/api/ApiService.kt
@@ -15,6 +15,7 @@ import com.barikoi.cnlapp.data.remote.models.RequestStockResponse
 import com.barikoi.cnlapp.data.remote.models.RouteResponse
 import com.barikoi.cnlapp.data.remote.models.SoResponse
 import com.barikoi.cnlapp.data.remote.models.StockRequestModel
+import com.barikoi.cnlapp.data.remote.models.TodaySummaryResponse
 import com.barikoi.cnlapp.data.remote.models.request.StockApprovalRequest
 import okhttp3.RequestBody
 import retrofit2.Response
@@ -63,6 +64,8 @@ interface ApiService {
         @Query("with_outlets") filterWithOutlet: String
     ): Response<RouteResponse>
 
+
+
     @GET("api/v1/get-so")
     suspend fun getSoList(): Response<SoResponse>
 
@@ -109,7 +112,10 @@ interface ApiService {
     ): Response<BaseResponse>
 
     @GET("api/v1/db-houses")
-    suspend fun getDHList(@Query("territory_id") territoryId: String): Response<DbHousesResponse>
+    suspend fun getDHList(
+        @Query("territory_id") territoryId: String?,
+        @Query("region_id") regionId: String?
+    ): Response<DbHousesResponse>
 
     @GET("api/v1/db-houses")
     suspend fun sendStockRequest(@Query("territory_id") territoryId: String): Response<DbHousesResponse>
@@ -138,4 +144,12 @@ interface ApiService {
     suspend fun saveGifts(
         @Body body: RequestBody
     ): Response<BaseResponse>
+
+
+    @GET("api/v1/get-to")
+    suspend fun getTodaySummary(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("today_summary") todaySummary: String
+    ): Response<TodaySummaryResponse>
 }
