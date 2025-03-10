e185e8e7fdb59e4ac11cbadc7579947f5a68acfd
Miraz <mmrrahman508@gmail.com>
2025-03-10 13:59:05 +0600

    today summary changes and add api calling for to list

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/TodaySummaryResponse.kt
@@ -0,0 +1,41 @@
+package com.barikoi.cnlapp.data.remote.models
+
+
+import android.annotation.SuppressLint
+import com.google.gson.annotations.SerializedName
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+data class TodaySummaryResponse(
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("status_code")
+    val statusCode: Int,
+    @SerializedName("to_list")
+    val toList: List<To>
+)
+
+data class To(
+    @SerializedName("num_of_sku")
+    val numOfSku: Int,
+    @SerializedName("num_of_visits")
+    val numOfVisits: Int,
+    @SerializedName("oneToHundred")
+    val oneToHundred: Int,
+    @SerializedName("to_id")
+    val toId: Int,
+    @SerializedName("user_name")
+    val toName: String,
+    @SerializedName("total_bounced_amount")
+    val totalBouncedAmount: String,
+    @SerializedName("total_ordered_amount")
+    val totalOrderedAmount: String,
+    @SerializedName("total_orders")
+    val totalOrders: Int
+) {
+    @SuppressLint("DefaultLocale")
+    fun totalAmountFormatted(): String {
+        return BigDecimal(totalOrderedAmount.toDouble()).setScale(2, RoundingMode.HALF_UP)
+            .toString()
+    }
+}
