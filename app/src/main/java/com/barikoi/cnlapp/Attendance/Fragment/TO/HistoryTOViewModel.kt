ded6159d77180d2338833c92881bad884e34bac7
Miraz <mmrrahman508@gmail.com>
2025-03-11 15:09:12 +0600

    feat: add HistoryTOViewModel for managing historical summary and SO data retrieval

---
+++ app/src/main/java/com/barikoi/cnlapp/Attendance/Fragment/TO/HistoryTOViewModel.kt
@@ -0,0 +1,55 @@
+package com.barikoi.cnlapp.Attendance.Fragment.TO
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.data.remote.models.SoResponseX
+import com.barikoi.cnlapp.data.remote.models.TodaySummaryResponse
+import com.barikoi.cnlapp.data.remote.repository.SoRepository
+import com.barikoi.cnlapp.data.remote.repository.SummaryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryTOViewModel @Inject constructor(
+    private val summaryRepository: SummaryRepository,
+    private val soRepository: SoRepository
+) : ViewModel() {
+
+    private val _toResponse = MutableLiveData<ApiState<TodaySummaryResponse>>()
+    val toResponse: LiveData<ApiState<TodaySummaryResponse>> = _toResponse
+
+
+    private val _soResponse = MutableLiveData<ApiState<SoResponseX>>()
+    val soResponse: LiveData<ApiState<SoResponseX>> = _soResponse
+
+
+    fun getTodaySummary(startDate: String, endDate: String, todaySummary: String) {
+        viewModelScope.launch {
+            summaryRepository.getTodaySummary(startDate, endDate, todaySummary)
+                .onStart {
+                    _toResponse.value = ApiState.Loading()
+                }
+                .collectLatest {
+                    _toResponse.value = it
+                }
+        }
+    }
+
+    fun getSoByTo(toId: String) {
+        viewModelScope.launch {
+            soRepository.getSoByTo(toId)
+                .onStart {
+                    _soResponse.value = ApiState.Loading()
+                }
+                .collectLatest {
+                    _soResponse.value = it
+                }
+        }
+    }
+}
