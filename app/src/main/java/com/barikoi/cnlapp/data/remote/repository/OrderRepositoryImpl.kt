9f799875b6b7b85f02450ba0f16233739c7b0b29
Miraz <mmrrahman508@gmail.com>
2025-05-26 11:11:18 +0600

    Add OrderRepository for saving orders
    This commit introduces the `OrderRepository` interface and its implementation `OrderRepositoryImpl`.
    
    The repository provides methods for:
    - `saveOrder`: Saves an order.
    - `saveNoOrder`: Handles cases where no order is placed.
    
    Both methods interact with the `ApiService` to make network requests and emit `ApiState` objects to represent the outcome (Success or Error). Error handling includes capturing exceptions with Sentry and logging.

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/OrderRepositoryImpl.kt
@@ -0,0 +1,88 @@
+package com.barikoi.cnlapp.data.remote.repository
+
+import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.base.api.Failure
+import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
+import com.barikoi.cnlapp.data.remote.api.ApiService
+import com.barikoi.cnlapp.data.remote.models.BaseResponse
+import com.barikoi.cnlapp.utils.AppLogger
+import io.sentry.Sentry
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.RequestBody
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+interface OrderRepository {
+    fun saveOrder(body: RequestBody): Flow<ApiState<BaseResponse>>
+    fun saveNoOrder(
+        body: RequestBody
+    ): Flow<ApiState<BaseResponse>>
+}
+
+class OrderRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : OrderRepository {
+
+    override fun saveOrder(body: RequestBody): Flow<ApiState<BaseResponse>> {
+        return flow {
+            try {
+                val response = apiService.saveOrder(body)
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
+    override fun saveNoOrder(
+        body: RequestBody
+    ): Flow<ApiState<BaseResponse>> {
+        return flow {
+            try {
+                val response = apiService.saveOrder(body)
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+}
