942a9f71ba00c0d26cc9ec05a7144dd767793c0e
Miraz <mmrrahman508@gmail.com>
2025-05-27 12:33:41 +0600

    Remove unnecessary logging and simplify dialog background drawable
    Removed debugging logs related to data processing in `processData` and camera activity result handling.
    Simplified setting the dialog background to transparent using `Color.TRANSPARENT.toDrawable()`.

--- app/src/main/java/com/barikoi/cnlapp/ui/add_gift/AddGiftActivity.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/add_gift/AddGiftActivity.kt
@@ -1,16 +1,13 @@
 package com.barikoi.cnlapp.ui.add_gift
 
-import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
@@ -18,6 +15,7 @@ import android.widget.LinearLayout
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -34,7 +32,6 @@ import com.barikoi.cnlapp.databinding.DialogConfirmGiftBinding
 import com.barikoi.cnlapp.ui.add_gift.adapter.AdapterMainGift
 import com.barikoi.cnlapp.ui.add_gift.vm.AddGiftViewModel
 import com.barikoi.cnlapp.utils.AppLogger
-import com.barikoi.cnlapp.utils.Constants
 import com.barikoi.cnlapp.utils.SharePrefUtils
 import com.barikoi.cnlapp.utils.extension.isViewEnable
 import com.barikoi.cnlapp.utils.extension.setHapticClickListener
@@ -256,17 +253,9 @@ class AddGiftActivity : BaseActivity() {
     }
 
     private fun processData(data: List<Gift>): List<GiftModel> {
-        Log.d("GiftList", "Received data: ${data.size}") // Debugging log
-
         val groupedData = data.groupBy { it.categoryId }
-        Log.d("GroupedGifts", "Grouped data: $groupedData") // Debugging log
 
-        return groupedData.map { (categoryId, gifts) ->
-            Log.d(
-                "CategoryProcessing",
-                "Processing category: $categoryId with ${gifts.size} gifts"
-            ) // Debugging log
-
+        return groupedData.map { (_, gifts) ->
             GiftModel(
                 title = gifts.first().category.name, // Using category name as title
                 gifts = gifts
@@ -305,11 +294,7 @@ class AddGiftActivity : BaseActivity() {
     private var startCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            AppLogger.log("startCameraNew:: $imageFilePath")
-            AppLogger.log("startCameraNew:: ${result.data}")
-
-            AppLogger.log("IMAGE_PICKER:: ${sharePrefUtils.getString(Constants.IMAGE_PICKER)}")
+        if (result.resultCode == RESULT_OK) {
 
             imageFiles.add(imageFilePath!!.path)
 
@@ -340,7 +325,7 @@ class AddGiftActivity : BaseActivity() {
 
         val dialog = Dialog(this)
         dialog.setCancelable(false)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(dialogBinding.root)
 
@@ -363,10 +348,6 @@ class AddGiftActivity : BaseActivity() {
         dialogBinding.llImagePickerView.rvImage.adapter = adapterImage
         dialogBinding.llImagePickerView.rvImage.layoutManager = layoutManager
         adapterImage.updateImages(imageFiles)
-
-
-
-
 
         dialogBinding.btnConfirm.setOnClickListener {
             if (imageFiles.isEmpty()) {
