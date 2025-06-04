866eefa35b7aadc680ac036daa40b143474f355c
Miraz <mmrrahman508@gmail.com>
2025-06-04 17:31:01 +0600

    Add ShopRepository for fetching shop list
    This commit introduces `ShopRepository` and its implementation `ShopRepositoryImpl`.
    
    `ShopRepository` provides a method `getShopList` to fetch a list of shops based on user ID, route ID, and an outlet filter. It returns a `Flow<ApiState<OutletsResponse>>` to handle asynchronous data loading and error states.
    
    `ShopRepositoryImpl` implements the `getShopList` method by making a network call using `ApiService`. It handles successful responses and different error scenarios, including network issues and other exceptions, by emitting appropriate `ApiState` values. Sentry is used for exception tracking.

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/ShopRepositoryImpl.kt
@@ -0,0 +1,63 @@
+package com.barikoi.cnlapp.data.remote.repository
+
+import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.base.api.Failure
+import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
+import com.barikoi.cnlapp.data.remote.api.ApiService
+import com.barikoi.cnlapp.data.remote.models.OutletsResponse
+import com.barikoi.cnlapp.utils.AppLogger
+import io.sentry.Sentry
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+interface ShopRepository {
+    fun getShopList(
+        userId: String,
+        routeId: String,
+        filterWithOutlet: String
+    ): Flow<ApiState<OutletsResponse>>
+}
+
+class ShopRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : ShopRepository {
+    override fun getShopList(
+        userId: String,
+        routeId: String,
+        filterWithOutlet: String
+    ): Flow<ApiState<OutletsResponse>> {
+        return flow {
+            try {
+                val response = apiService.getOutlets(
+                    userId,
+                    routeId,
+                    filterWithOutlet
+                )
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+}
