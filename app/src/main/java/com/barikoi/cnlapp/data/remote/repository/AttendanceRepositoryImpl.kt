f4f8fffd578f8627c00b3686f8f66270a83319a4
Miraz <mmrrahman508@gmail.com>
2025-07-02 13:00:23 +0600

    Add AttendanceRepository and its implementation
    This commit introduces the `AttendanceRepository` interface and its implementation `AttendanceRepositoryImpl`.
    
    The repository provides a `checkAttendance` method that:
    - Takes `startDate` and `endDate` as parameters.
    - Makes an API call to the `checkAttendance` endpoint of `ApiService`.
    - Emits `ApiState.Success` with the `CheckAttendanceResponse` if the API call is successful.
    - Emits `ApiState.Error` with the appropriate `Failure` type if the API call fails or an exception occurs.
    - Handles `UnknownHostException` specifically by emitting `Failure.HTTP.NetworkConnection`.
    - Logs exceptions to Sentry.

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/AttendanceRepositoryImpl.kt
@@ -0,0 +1,63 @@
+package com.barikoi.cnlapp.data.remote.repository
+
+import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.base.api.Failure
+import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
+import com.barikoi.cnlapp.data.remote.api.ApiService
+import com.barikoi.cnlapp.data.remote.models.BaseResponse
+import com.barikoi.cnlapp.data.remote.models.CheckAttendanceResponse
+import com.barikoi.cnlapp.data.remote.models.GiftResponse
+import com.barikoi.cnlapp.utils.AppLogger
+import io.sentry.Sentry
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.RequestBody
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+interface AttendanceRepository {
+    fun checkAttendance(
+        startDate: String,
+        endDate: String,
+    ): Flow<ApiState<CheckAttendanceResponse>>
+}
+
+class AttendanceRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : AttendanceRepository {
+    override fun checkAttendance(
+        startDate: String,
+        endDate: String
+    ): Flow<ApiState<CheckAttendanceResponse>> {
+        return flow {
+            try {
+                val response = apiService.checkAttendance(
+                    startDate = startDate,
+                    endDate = endDate
+                )
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+}
