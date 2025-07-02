579d5eacfb172a9492c4f6c936f3ffdcf05a2148
Miraz <mmrrahman508@gmail.com>
2025-07-02 13:02:44 +0600

    Add `getPreviousDayOrder` to `OrderRepository`
    This commit introduces a new function `getPreviousDayOrder` to the `OrderRepository` interface and its implementation `OrderRepositoryImpl`. This function retrieves previous day's order information based on `userId` and `orderId`.
    
    Additionally, a bug in `saveNoOrder` has been fixed where it was incorrectly calling `apiService.saveOrder` instead of `apiService.saveNoOrder`.

--- app/src/main/java/com/barikoi/cnlapp/data/remote/repository/OrderRepositoryImpl.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/OrderRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.barikoi.cnlapp.base.api.Failure
 import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
 import com.barikoi.cnlapp.data.remote.api.ApiService
 import com.barikoi.cnlapp.data.remote.models.BaseResponse
+import com.barikoi.cnlapp.data.remote.models.pre_order.PreviousDayOrderResponse
 import com.barikoi.cnlapp.utils.AppLogger
 import io.sentry.Sentry
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +16,10 @@ import javax.inject.Inject
 
 interface OrderRepository {
     fun saveOrder(body: RequestBody): Flow<ApiState<BaseResponse>>
+    fun getPreviousDayOrder(
+        userId: String,
+        orderId: String,
+    ): Flow<ApiState<PreviousDayOrderResponse>>
     fun saveNoOrder(
         body: RequestBody
     ): Flow<ApiState<BaseResponse>>
@@ -54,12 +59,45 @@ class OrderRepositoryImpl @Inject constructor(
         }
     }
 
+    override fun getPreviousDayOrder(
+        userId: String,
+        orderId: String
+    ): Flow<ApiState<PreviousDayOrderResponse>> {
+        return flow {
+            try {
+                val response = apiService.getPreviousDayOrder(userId, orderId, "1")
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
     override fun saveNoOrder(
         body: RequestBody
     ): Flow<ApiState<BaseResponse>> {
         return flow {
             try {
-                val response = apiService.saveOrder(body)
+                val response = apiService.saveNoOrder(body)
                 if (response.isSuccessful) {
                     emit(ApiState.Success(response.body()!!))
                 } else {
