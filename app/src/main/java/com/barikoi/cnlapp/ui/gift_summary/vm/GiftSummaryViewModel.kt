964dbdbc84b953ec00ca320aec0795ae01cfc93f
Miraz <mmrrahman508@gmail.com>
2025-07-06 18:00:56 +0600

    Add `GiftRepository` and `getGiftSummary` to `GiftSummaryViewModel`
    This commit introduces a new dependency, `GiftRepository`, to `GiftSummaryViewModel`.
    
    Additionally, a new function `getGiftSummary` is added. This function allows fetching gift summary data based on `userId`, `startDate`, and `endDate`, and updates a `LiveData` object `_giftSummaryResponse` with the API response.

--- app/src/main/java/com/barikoi/cnlapp/ui/gift_summary/vm/GiftSummaryViewModel.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/gift_summary/vm/GiftSummaryViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barikoi.cnlapp.base.api.ApiState
-import com.barikoi.cnlapp.data.remote.models.PendingResponse
+import com.barikoi.cnlapp.data.remote.models.GIftSummaryResponse
 import com.barikoi.cnlapp.data.remote.models.SoResponseX
 import com.barikoi.cnlapp.data.remote.models.TodaySummaryResponse
+import com.barikoi.cnlapp.data.remote.repository.GiftRepository
 import com.barikoi.cnlapp.data.remote.repository.SoRepository
 import com.barikoi.cnlapp.data.remote.repository.SummaryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,15 +19,18 @@ import javax.inject.Inject
 @HiltViewModel
 class GiftSummaryViewModel @Inject constructor(
     private val summaryRepository: SummaryRepository,
-    private val soRepository: SoRepository
+    private val soRepository: SoRepository,
+    private val giftRepository: GiftRepository
 ) : ViewModel() {
-
 
     private val _toResponse = MutableLiveData<ApiState<TodaySummaryResponse>>()
     val toResponse: LiveData<ApiState<TodaySummaryResponse>> = _toResponse
 
     private val _soResponse = MutableLiveData<ApiState<SoResponseX>>()
     val soResponse: LiveData<ApiState<SoResponseX>> = _soResponse
+
+    private val _giftSummaryResponse = MutableLiveData<ApiState<GIftSummaryResponse>>()
+    val giftSummaryResponse: LiveData<ApiState<GIftSummaryResponse>> = _giftSummaryResponse
 
     fun getToList() {
         viewModelScope.launch {
@@ -36,9 +40,7 @@ class GiftSummaryViewModel @Inject constructor(
                 _toResponse.postValue(response)
             }
         }
-
     }
-
 
     fun getSoList(userId: String) {
         viewModelScope.launch {
@@ -48,6 +50,20 @@ class GiftSummaryViewModel @Inject constructor(
                 _soResponse.postValue(response)
             }
         }
-
     }
+    fun getGiftSummary(
+        userId: String,
+        startDate: String?,
+        endDate: String?
+    ) {
+        viewModelScope.launch {
+            giftRepository.getGiftSummary(userId, startDate, endDate).onStart {
+                _giftSummaryResponse.postValue(ApiState.Loading())
+            }.collect { response ->
+                _giftSummaryResponse.postValue(response)
+            }
+        }
+    }
+
+
 }
