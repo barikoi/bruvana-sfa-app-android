78079405ae54edfcd8c54a7a7afd463cc10d6d7e
Miraz <mmrrahman508@gmail.com>
2025-07-07 18:05:03 +0600

    Add `getActiveInactiveUsers` to `AttendanceRepository`
    This commit introduces a new method `getActiveInactiveUsers` to the `AttendanceRepository` and its implementation `AttendanceRepositoryImpl`.
    
    This method fetches active and inactive user data within a specified date range from the `ApiService`. It emits `ApiState.Success` with the response body on successful API calls, or `ApiState.Error` with the corresponding error type on failure or exception.

--- app/src/main/java/com/barikoi/cnlapp/data/remote/repository/AttendanceRepositoryImpl.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/AttendanceRepositoryImpl.kt
@@ -4,14 +4,12 @@ import com.barikoi.cnlapp.base.api.ApiState
 import com.barikoi.cnlapp.base.api.Failure
 import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
 import com.barikoi.cnlapp.data.remote.api.ApiService
-import com.barikoi.cnlapp.data.remote.models.BaseResponse
 import com.barikoi.cnlapp.data.remote.models.CheckAttendanceResponse
-import com.barikoi.cnlapp.data.remote.models.GiftResponse
+import com.barikoi.cnlapp.data.remote.models.active.ActiveInactiveUserResponse
 import com.barikoi.cnlapp.utils.AppLogger
 import io.sentry.Sentry
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import okhttp3.RequestBody
 import java.net.UnknownHostException
 import javax.inject.Inject
 
@@ -20,6 +18,12 @@ interface AttendanceRepository {
         startDate: String,
         endDate: String,
     ): Flow<ApiState<CheckAttendanceResponse>>
+
+
+    fun getActiveInactiveUsers(
+        startDate: String,
+        endDate: String,
+    ): Flow<ApiState<ActiveInactiveUserResponse>>
 }
 
 class AttendanceRepositoryImpl @Inject constructor(
@@ -32,6 +36,42 @@ class AttendanceRepositoryImpl @Inject constructor(
         return flow {
             try {
                 val response = apiService.checkAttendance(
+                    startDate = startDate,
+                    endDate = endDate
+                )
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code())
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
+    override fun getActiveInactiveUsers(
+        startDate: String,
+        endDate: String
+    ): Flow<ApiState<ActiveInactiveUserResponse>> {
+        return flow {
+            try {
+                val response = apiService.getActiveInactive(
                     startDate = startDate,
                     endDate = endDate
                 )
