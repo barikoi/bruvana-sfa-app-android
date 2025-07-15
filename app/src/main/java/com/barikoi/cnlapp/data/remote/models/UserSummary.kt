179f86864ba23d8d4e7d9a63854f8fd5ca435e84
Miraz <mmrrahman508@gmail.com>
2025-07-15 12:58:04 +0600

    Add UserSummary data class
    This commit introduces the `UserSummary` data class, which is `Parcelable`. It includes the following properties:
    
    - `id`: String
    - `name`: String
    - `userType`: String
    - `totalOrders`: Double
    - `ads`: Double

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/UserSummary.kt
@@ -0,0 +1,14 @@
+package com.barikoi.cnlapp.data.remote.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserSummary(
+    val id: String,
+    val name: String,
+    val userType: String,
+    val totalOrders: Double,
+    val ads: Double
+) : Parcelable
+
