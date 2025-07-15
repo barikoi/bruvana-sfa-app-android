1d7376a6a19b92468fa7ca50f7c919671781ea41
Miraz <mmrrahman508@gmail.com>
2025-07-15 13:00:06 +0600

    Add SO summary and overview stats to SummaryRepository
    This commit introduces the following new methods to `SummaryRepository` and its implementation `SummaryRepositoryImpl`:
    
    - `getSoWithTodaySummary`: Fetches SO (Sales Officer) summary data based on a date range and TO (Territory Officer) ID.
    - `getOverViewStatsTO`: Retrieves overview statistics for a TO based on a date range, territory ID, and user ID.
    - `getOverViewStatsASM`: Retrieves overview statistics for an ASM (Area Sales Manager) based on a date range, region ID, and user ID.
    
    These additions expand the repository's capabilities to provide more detailed sales and operational insights.

--- app/src/main/java/com/barikoi/cnlapp/data/remote/repository/SummaryRepositoryImpl.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/repository/SummaryRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package com.barikoi.cnlapp.data.remote.repository
 
-import com.barikoi.cnlapp.BuildConfig
 import com.barikoi.cnlapp.base.api.ApiState
 import com.barikoi.cnlapp.base.api.Failure
 import com.barikoi.cnlapp.base.api.getErrorTypeByHTTPCode
 import com.barikoi.cnlapp.data.remote.api.ApiService
+import com.barikoi.cnlapp.data.remote.models.BaseResponse
 import com.barikoi.cnlapp.data.remote.models.TodaySummaryResponse
+import com.barikoi.cnlapp.data.remote.models.active.OverViewStatsResponse
+import com.barikoi.cnlapp.data.remote.models.so.SoWithSummaryResponse
 import com.barikoi.cnlapp.utils.AppLogger
 import com.google.gson.Gson
 import io.sentry.Sentry
@@ -18,6 +20,24 @@ interface SummaryRepository {
     suspend fun getTodaySummary(
         startDate: String?, endDate: String?, todaySummary: String?
     ): Flow<ApiState<TodaySummaryResponse>>
+
+    suspend fun getSoWithTodaySummary(
+        startDate: String, endDate: String, toID: String
+    ): Flow<ApiState<SoWithSummaryResponse>>
+
+    suspend fun getOverViewStatsTO(
+        startDate: String,
+        endDate: String,
+        territoryId: String,
+        userId: String
+    ): Flow<ApiState<OverViewStatsResponse>>
+
+    suspend fun getOverViewStatsASM(
+        startDate: String,
+        endDate: String,
+        regionId: String,
+        userId: String
+    ): Flow<ApiState<OverViewStatsResponse>>
 }
 
 class SummaryRepositoryImpl @Inject constructor(
@@ -37,6 +57,123 @@ class SummaryRepositoryImpl @Inject constructor(
                             getErrorTypeByHTTPCode(response.code()),
                             errorResponse = Gson().fromJson(
                                 response.errorBody()?.string(), TodaySummaryResponse::class.java
+                            )
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
+    override suspend fun getSoWithTodaySummary(
+        startDate: String,
+        endDate: String,
+        toID: String
+    ): Flow<ApiState<SoWithSummaryResponse>> {
+        return flow {
+            try {
+                val response = apiService.getSoWithTodaySummary(startDate, endDate, toID)
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code()),
+                            errorResponse = Gson().fromJson(
+                                response.errorBody()?.string(), SoWithSummaryResponse::class.java
+                            )
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
+    override suspend fun getOverViewStatsTO(
+        startDate: String,
+        endDate: String,
+        territoryId: String,
+        userId: String
+    ): Flow<ApiState<OverViewStatsResponse>> {
+        return flow {
+            try {
+                val response =
+                    apiService.getOverViewStatsTO(startDate, endDate, 1, territoryId, userId)
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code()),
+                            errorResponse = Gson().fromJson(
+                                response.errorBody()?.string(), OverViewStatsResponse::class.java
+                            )
+                        )
+                    )
+                }
+            } catch (exception: Throwable) {
+                Sentry.captureException(exception)
+
+                when (exception) {
+                    is UnknownHostException -> {
+                        emit(ApiState.Error((Failure.HTTP.NetworkConnection)))
+                    }
+
+                    else -> {
+                        emit(ApiState.Error(Failure.Exception(exception)))
+                    }
+                }
+                AppLogger.log(exception.toString())
+            }
+        }
+    }
+
+    override suspend fun getOverViewStatsASM(
+        startDate: String,
+        endDate: String,
+        regionId: String,
+        userId: String
+    ): Flow<ApiState<OverViewStatsResponse>> {
+        return flow {
+            try {
+                val response = apiService.getOverViewStatsASM(
+                    startDate, endDate,
+                    1, regionId, userId
+                )
+                if (response.isSuccessful) {
+                    emit(ApiState.Success(response.body()!!))
+                } else {
+                    emit(
+                        ApiState.Error(
+                            getErrorTypeByHTTPCode(response.code()),
+                            errorResponse = Gson().fromJson(
+                                response.errorBody()?.string(), OverViewStatsResponse::class.java
                             )
                         )
                     )
