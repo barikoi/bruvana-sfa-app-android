0bdbe040fe7cc3805cd729ba79cecdd5e495c30a
Miraz <mmrrahman508@gmail.com>
2025-07-15 13:03:31 +0600

    Add ActiveInactiveUserResponse and ActiveInactiveUser data classes
    This commit introduces two new data classes:
    
    - `ActiveInactiveUserResponse`: Represents the API response for active and inactive users. It contains lists of `ActiveInactiveUser` for both active and inactive users, along with a `statusCode`.
    - `ActiveInactiveUser`: Represents an individual user with details such as check-in/out times, location, route information, and user metadata. It is Parcelable for easier data transfer.

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/ActiveInactiveUserResponse.kt
@@ -0,0 +1,52 @@
+package com.barikoi.cnlapp.data.remote.models
+
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+data class ActiveInactiveUserResponse(
+    @SerializedName("active")
+    val active: List<ActiveInactiveUser>,
+    @SerializedName("inactive")
+    val inactive: List<ActiveInactiveUser>,
+    @SerializedName("status_code")
+    val statusCode: Int
+)
+
+
+@Parcelize
+data class ActiveInactiveUser(
+    @SerializedName("checkin_address")
+    val checkinAddress: String?,
+    @SerializedName("checkin_time")
+    val checkinTime: String?,
+    @SerializedName("checkout_time")
+    val checkoutTime: String?,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: List<Image>?,
+    @SerializedName("is_absent")
+    val isAbsent: Int,
+    @SerializedName("is_late")
+    val isLate: Int,
+    @SerializedName("latitude")
+    val latitude: String?,
+    @SerializedName("longitude")
+    val longitude: String?,
+    @SerializedName("remarks")
+    val remarks: String?,
+    @SerializedName("route_id")
+    val routeId: Int?,
+    @SerializedName("route_name")
+    val routeName: String?,
+    @SerializedName("updated_at")
+    val updatedAt: String,
+    @SerializedName("user_id")
+    val userId: Int,
+    @SerializedName("user_name")
+    val userName: String
+) : Parcelable
