331ced6092441929935f84f45c3665ba8ba13a0c
Miraz <mmrrahman508@gmail.com>
2025-07-15 13:02:35 +0600

    Change RouteViewModel import path in RouteFragment
    The import path for `RouteViewModel` has been updated from `com.barikoi.cnlapp.Activity.RouteViewModel` to `com.barikoi.cnlapp.ui.route.RouteViewModel`.

--- app/src/production/java/com/barikoi/cnlapp/Fragment/RouteFragment.kt
+++ app/src/production/java/com/barikoi/cnlapp/Fragment/RouteFragment.kt
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.barikoi.cnlapp.Activity.RouteViewModel
 import com.barikoi.cnlapp.Adapter.RouteListAdapter
 import com.barikoi.cnlapp.base.api.ApiState
 import com.barikoi.cnlapp.base.api.NetworkFailureMessage
 import com.barikoi.cnlapp.databinding.FragmentRouteBinding
+import com.barikoi.cnlapp.ui.route.RouteViewModel
 import com.barikoi.cnlapp.utils.Api
 import com.barikoi.cnlapp.utils.AppLogger
 import com.barikoi.cnlapp.utils.SharePrefUtils
