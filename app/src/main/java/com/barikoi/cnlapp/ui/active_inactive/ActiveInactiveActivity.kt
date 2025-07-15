b2365dd134496539ec2c2d33cebdc89fcdf209b5
Miraz <mmrrahman508@gmail.com>
2025-07-15 13:03:12 +0600

    Refactor: Populate RecyclerView with Active/Inactive User Data
    This commit updates `ActiveInactiveActivity` to display a list of active or inactive users using a `RecyclerView`.
    
    Key changes:
    - Initializes and sets up `AdapterActiveInactive` for the `RecyclerView`.
    - Retrieves the user list (passed as `ActiveInactiveUser` ParcelableArrayList) from the intent.
    - Populates the adapter with the retrieved user data.
    - Logs a message if the user list is null.
    - Sets the toolbar title based on the "so_status" extra from the intent.

--- app/src/main/java/com/barikoi/cnlapp/ui/active_inactive/ActiveInactiveActivity.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/active_inactive/ActiveInactiveActivity.kt
@@ -2,12 +2,17 @@ package com.barikoi.cnlapp.ui.active_inactive
 
 import android.os.Bundle
 import com.barikoi.cnlapp.base.ac.BaseActivity
+import com.barikoi.cnlapp.data.remote.models.ActiveInactiveUser
 import com.barikoi.cnlapp.databinding.ActivityActiveInactive2Binding
+import com.barikoi.cnlapp.utils.AppLogger
+import com.barikoi.cnlapp.utils.extension.getParcelableArrayListCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActiveInactiveActivity : BaseActivity() {
     private lateinit var binding: ActivityActiveInactive2Binding
+
+    private lateinit var adapterActiveInactive: AdapterActiveInactive
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,20 @@ class ActiveInactiveActivity : BaseActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
 
+        adapterActiveInactive = AdapterActiveInactive()
+
         binding.toolbar.tvTitle.text = intent.getStringExtra("so_status")
+
+        val users = intent.getParcelableArrayListCompat<ActiveInactiveUser>("users")
+
+        binding.rcvActiveInactiveUsers.layoutManager =
+            androidx.recyclerview.widget.LinearLayoutManager(this)
+        binding.rcvActiveInactiveUsers.adapter = adapterActiveInactive
+        if (users != null) {
+            adapterActiveInactive.updateList(users)
+        } else {
+            AppLogger.log("ActiveInactiveActivity::onCreate users is null")
+        }
+
     }
 }
