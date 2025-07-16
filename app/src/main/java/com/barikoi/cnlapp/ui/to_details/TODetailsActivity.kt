2980573810a81c45143d889762ad25f8702bd85c
Miraz <mmrrahman508@gmail.com>
2025-07-16 18:06:13 +0600

    Integrate TO Details into TODetailsActivity
    This commit modifies `TODetailsActivity` to:
    - Retrieve `UserSummary` data passed via intent.
    - Set the toolbar title to the TO's name.
    - Implement a back button with haptic feedback.
    - Pass "TO", `territoryId`, and `userId` to the `HomeFragment` to display TO-specific data.

--- app/src/main/java/com/barikoi/cnlapp/ui/to_details/TODetailsActivity.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/to_details/TODetailsActivity.kt
@@ -3,8 +3,11 @@ package com.barikoi.cnlapp.ui.to_details
 import android.os.Bundle
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.base.ac.BaseActivity
+import com.barikoi.cnlapp.data.remote.models.UserSummary
 import com.barikoi.cnlapp.databinding.ActivityTodetailsBinding
 import com.barikoi.cnlapp.ui.home.HomeFragment
+import com.barikoi.cnlapp.utils.extension.parcelable
+import com.barikoi.cnlapp.utils.extension.setHapticClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,9 +19,17 @@ class TODetailsActivity : BaseActivity() {
         binding = ActivityTodetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val user = intent.parcelable<UserSummary>("user_summary")
+
+        binding.toolbar.tvTitle.text = user?.name.toString()
+        binding.toolbar.btnBack.setHapticClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.container, HomeFragment())
+        fragmentTransaction.replace(R.id.container, HomeFragment("TO", user!!.territoryId.toString(), user.id))
         fragmentTransaction.commit()
     }
 }
