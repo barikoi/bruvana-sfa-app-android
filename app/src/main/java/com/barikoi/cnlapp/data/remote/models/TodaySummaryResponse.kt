2365c00e3eec55aae03ebf9b63436383662bf9a5
Miraz <mmrrahman508@gmail.com>
2025-07-16 18:06:06 +0600

    Refactor TodaySummaryResponse model
    - Added `territoryId` and `activeInactiveData` fields to the `TodaySummaryResponse` data class.
    - Introduced a new data class `ActiveInactiveData` to represent active and inactive counts.
    - Updated the `toTodaySummary()` extension function to include the new fields.

--- app/src/main/java/com/barikoi/cnlapp/data/remote/models/TodaySummaryResponse.kt
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/TodaySummaryResponse.kt
@@ -43,7 +43,12 @@ data class To(
     @SerializedName("total_orders")
     val totalOrders: Int,
     @SerializedName("unique_outlet_count")
-    val uniqueOutletCount: Int
+    val uniqueOutletCount: Int,
+    @SerializedName("territory_id")
+    val territoryId: Int,
+    @SerializedName("activeInactiveData")
+    val activeInactiveData: ActiveInactiveData
+
 ) {
     @SuppressLint("DefaultLocale")
     fun totalAmountFormatted(): String {
@@ -57,7 +62,17 @@ data class To(
             name = toName,
             userType = "TO",
             totalOrders = totalOrders.toDouble(),
-            ads = BigDecimal(ads).setScale(2, RoundingMode.HALF_UP).toDouble()
+            ads = BigDecimal(ads).setScale(2, RoundingMode.HALF_UP).toDouble(),
+            territoryId = territoryId,
+            active = activeInactiveData.active,
+            inactive = activeInactiveData.inactive
         )
     }
 }
+
+data class ActiveInactiveData(
+    @SerializedName("active")
+    val active: Int,
+    @SerializedName("inactive")
+    val inactive: Int
+)
