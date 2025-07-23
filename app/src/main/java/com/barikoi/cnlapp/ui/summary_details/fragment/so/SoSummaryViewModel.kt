93431d6bccfd7c34c38d64292cb9c98665409b81
Miraz <mmrrahman508@gmail.com>
2025-07-23 10:47:52 +0600

    Add SoSummaryViewModel for fetching and managing summary data
    This commit introduces `SoSummaryViewModel`, an HiltViewModel responsible for:
    
    - Fetching SO (Sales Officer) summary statistics using `getSOSummary`.
    - Fetching outlet type summary data using `getOutletTypeSummary`.
    
    Both methods interact with `SummaryRepository` and expose the results as `LiveData<ApiState>` objects (`soStatsResponse` and `outletTypeSummaryResponse` respectively) to observe API loading, success, and error states.

---
+++ app/src/main/java/com/barikoi/cnlapp/ui/summary_details/fragment/so/SoSummaryViewModel.kt
@@ -0,0 +1,63 @@
+package com.barikoi.cnlapp.ui.summary_details.fragment.so
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.barikoi.cnlapp.base.api.ApiState
+import com.barikoi.cnlapp.data.remote.models.OutletTypeSummaryResponse
+import com.barikoi.cnlapp.data.remote.models.active.OverViewStatsResponse
+import com.barikoi.cnlapp.data.remote.repository.SummaryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SoSummaryViewModel @Inject constructor(
+    private val summaryRepository: SummaryRepository
+) : ViewModel() {
+
+    private val _soStatsResponse = MutableLiveData<ApiState<OverViewStatsResponse>>()
+    val soStatsResponse: LiveData<ApiState<OverViewStatsResponse>> =
+        _soStatsResponse
+
+    private val _outletTypeSummaryResponse = MutableLiveData<ApiState<OutletTypeSummaryResponse>>()
+    val outletTypeSummaryResponse: LiveData<ApiState<OutletTypeSummaryResponse>> =
+        _outletTypeSummaryResponse
+
+
+    fun getSOSummary(
+        startDate: String,
+        endDate: String,
+        userId: String
+    ) {
+        viewModelScope.launch {
+            summaryRepository.getSoSummary(
+                startDate, endDate, userId
+            ).onStart {
+                _soStatsResponse.value = ApiState.Loading()
+            }
+                .collect {
+                    _soStatsResponse.value = it
+                }
+        }
+    }
+
+    fun getOutletTypeSummary(
+        startDate: String,
+        endDate: String,
+        userId: String
+    ) {
+        viewModelScope.launch {
+            summaryRepository.getOutletTypeSummary(
+                startDate, endDate, userId
+            ).onStart {
+                _outletTypeSummaryResponse.value = ApiState.Loading()
+            }.collect {
+                _outletTypeSummaryResponse.value = it
+            }
+        }
+    }
+}
