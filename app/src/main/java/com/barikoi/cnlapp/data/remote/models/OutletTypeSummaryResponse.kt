b9ea0700b368aafe625a9e4312b70c276961b27e
Miraz <mmrrahman508@gmail.com>
2025-07-23 10:52:42 +0600

    Add OutletTypeSummaryResponse model
    This commit introduces the `OutletTypeSummaryResponse` data class to represent the response for outlet type summaries. It includes nested data classes `OutletTypeData` to hold individual outlet type information such as AIV, contribution, delivery amount, gross amount, visit counts, order amount, and total outlets.
    
    An `isExpanded` property has been added to `OutletTypeData` for UI state management.

---
+++ app/src/main/java/com/barikoi/cnlapp/data/remote/models/OutletTypeSummaryResponse.kt
@@ -0,0 +1,40 @@
+package com.barikoi.cnlapp.data.remote.models
+
+import com.google.gson.annotations.SerializedName
+
+data class OutletTypeSummaryResponse(
+    @SerializedName("data")
+    val `data`: List<OutletTypeData>,
+    @SerializedName("msg")
+    val msg: String,
+    @SerializedName("status")
+    val status: String
+)
+
+
+data class OutletTypeData(
+    @SerializedName("aiv")
+    val aiv: String,
+    @SerializedName("contribution")
+    val contribution: Double,
+    @SerializedName("delivery_amount")
+    val deliveryAmount: String?,
+    @SerializedName("gross_amount")
+    val grossAmount: String,
+    @SerializedName("non_productive_visits")
+    val nonProductiveVisits: Int,
+    @SerializedName("order_amount")
+    val orderAmount: String,
+    @SerializedName("outlet_type")
+    val outletType: String,
+    @SerializedName("productive_visits")
+    val productiveVisits: Int,
+    @SerializedName("qty_presence")
+    val qtyPresence: String,
+    @SerializedName("total_outlet")
+    val totalOutlet: Int,
+    @SerializedName("total_visits")
+    val totalVisits: Int,
+
+    var isExpanded: Boolean = false
+)
