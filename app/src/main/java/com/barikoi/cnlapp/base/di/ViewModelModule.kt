35f398970631d1544db45c714e780117450a5389
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:49:44 +0600

    Reorder repository imports in ViewModelModule
    The `GiftRepository` and `RouteRepository` related imports have been moved to maintain alphabetical order within the `com.barikoi.cnlapp.data.remote.repository` package imports.

--- app/src/main/java/com/barikoi/cnlapp/base/di/ViewModelModule.kt
+++ app/src/main/java/com/barikoi/cnlapp/base/di/ViewModelModule.kt
@@ -2,17 +2,15 @@ package com.barikoi.cnlapp.base.di
 
 import android.content.Context
 import com.barikoi.cnlapp.data.remote.api.ApiService
-import com.barikoi.cnlapp.data.remote.repository.RouteRepository
-import com.barikoi.cnlapp.data.remote.repository.RouteRepositoryImpl
 import com.barikoi.cnlapp.data.remote.api.TraceApiService
-import com.barikoi.cnlapp.data.remote.repository.GiftRepository
-import com.barikoi.cnlapp.data.remote.repository.GiftRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.AttendanceRepository
 import com.barikoi.cnlapp.data.remote.repository.AttendanceRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.AuthRepository
 import com.barikoi.cnlapp.data.remote.repository.AuthRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.DownloadRepository
 import com.barikoi.cnlapp.data.remote.repository.DownloadRepositoryImpl
+import com.barikoi.cnlapp.data.remote.repository.GiftRepository
+import com.barikoi.cnlapp.data.remote.repository.GiftRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.NotificationRepository
 import com.barikoi.cnlapp.data.remote.repository.NotificationRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.OfferRepository
@@ -25,6 +23,8 @@ import com.barikoi.cnlapp.data.remote.repository.ProductStockRepository
 import com.barikoi.cnlapp.data.remote.repository.ProductStockRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.ReverseGeoRepository
 import com.barikoi.cnlapp.data.remote.repository.ReverseGeoRepositoryImpl
+import com.barikoi.cnlapp.data.remote.repository.RouteRepository
+import com.barikoi.cnlapp.data.remote.repository.RouteRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.ShopRepository
 import com.barikoi.cnlapp.data.remote.repository.ShopRepositoryImpl
 import com.barikoi.cnlapp.data.remote.repository.SoRepository
