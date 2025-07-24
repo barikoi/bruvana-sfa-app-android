03fd7c838f9b2671a5c6f335534d40e6b3f358ee
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:25:14 +0600

    Remove unused import BaseResponse from HomeViewModel
    The `BaseResponse` class was not being used in the `HomeViewModel.kt` file, so it has been removed.

--- app/src/main/java/com/barikoi/cnlapp/ui/home/vm/HomeViewModel.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/home/vm/HomeViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barikoi.cnlapp.base.api.ApiState
-import com.barikoi.cnlapp.data.remote.models.BaseResponse
 import com.barikoi.cnlapp.data.remote.models.ActiveInactiveUserResponse
 import com.barikoi.cnlapp.data.remote.models.TodaySummaryResponse
 import com.barikoi.cnlapp.data.remote.models.active.OverViewStatsResponse
