b578a1a03cbd46ae29f941f7f1fcbfb990febcd8
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:47:16 +0600

    Refactor: Reorder imports in SummaryFragment.kt
    The import statements in `SummaryFragment.kt` have been reordered for better organization and adherence to Kotlin style guidelines.

--- app/src/main/java/com/barikoi/cnlapp/Attendance/Fragment/SO/SummaryFragment.kt
+++ app/src/main/java/com/barikoi/cnlapp/Attendance/Fragment/SO/SummaryFragment.kt
@@ -12,10 +12,10 @@ import androidx.preference.PreferenceManager
 import com.android.volley.NetworkResponse
 import com.android.volley.RequestQueue
 import com.android.volley.VolleyError
-import com.barikoi.cnlapp.ui.main.MainActivity
 import com.barikoi.cnlapp.Attendance.Adapter.SO.ReasonListAdapter
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.databinding.FragmentSummaryBinding
+import com.barikoi.cnlapp.ui.main.MainActivity
 import com.barikoi.cnlapp.utils.Api
 import com.barikoi.cnlapp.utils.ApiService.ApiServiceListener
 import com.barikoi.cnlapp.utils.ApiService.ApiServices
