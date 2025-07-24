6caa2a00b93c5143ff669746e7f71ee51156ac3e
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:28:12 +0600

    Refactor NoticeListAdapter.kt
    This commit refactors `NoticeListAdapter.kt` with the following changes:
    - Replaced Glide with Coil for image loading.
    - Optimized imports.
    - Simplified ViewHolder initialization.
    - Improved code formatting and readability.

--- app/src/main/java/com/barikoi/cnlapp/Notice/NoticeListAdapter.kt
+++ app/src/main/java/com/barikoi/cnlapp/Notice/NoticeListAdapter.kt
@@ -7,51 +7,52 @@ import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil3.load
 import com.barikoi.cnlapp.R
-import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 
-class NoticeListAdapter (val notices: List<Notice>) : RecyclerView.Adapter<NoticeListAdapter.ViewHolder>(){
+class NoticeListAdapter(val notices: List<Notice>) :
+    RecyclerView.Adapter<NoticeListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): NoticeListAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.single_notice_view, parent, false)
+    ): ViewHolder {
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.single_notice_view, parent, false)
         return ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: NoticeListAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val mItem = notices[position]
 
         holder.userName.text = mItem.senderName
         holder.tvDesignation.text = mItem.designation
         holder.noticeMessage.text = mItem.message
 
-        if (!mItem.updated_at.equals("null")){
+        if (mItem.updated_at != "null") {
             val oldDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
             val df = SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH)
             val orderDate = df.format(oldDate.parse(mItem.updated_at))
-            holder.noticeDate.setText(orderDate)
+            holder.noticeDate.text = orderDate
         }
 
-        if (!mItem.imageUrl.isNullOrEmpty() && !mItem.imageUrl.equals("null")){
-            Glide.with(holder.itemView.context)
-                .load(mItem.imageUrl)
-                .into(holder.imageUser)
-        }else{
+        if (mItem.imageUrl.isNotEmpty() && mItem.imageUrl != "null") {
+            holder.imageUser.load(
+                mItem.imageUrl
+            )
+        } else {
             holder.imageUser.visibility = View.GONE
         }
 
-
-
         var expandable = false
 
-        holder.noticeMessage.getViewTreeObserver().addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+        holder.noticeMessage.viewTreeObserver.addOnGlobalLayoutListener(object :
+            OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 //Log.d("Notice", "Line Count: "+holder.noticeMessage.lineCount)
-                if (holder.noticeMessage.lineCount >2){
+                if (holder.noticeMessage.lineCount > 2) {
                     /*Log.d("Notice", "Line Count: "+holder.noticeMessage.lineCount+" ellipse: "+holder.noticeMessage.layout.getEllipsisCount(holder.noticeMessage.lineCount))
                     if (holder.noticeMessage.layout.getEllipsisCount(holder.noticeMessage.lineCount-1) > 3){
                         holder.seeMore.visibility = View.VISIBLE
@@ -62,19 +63,19 @@ class NoticeListAdapter (val notices: List<Notice>) : RecyclerView.Adapter<Notic
                     }*/
                     holder.seeMore.visibility = View.VISIBLE
                     holder.seeMore.text = holder.itemView.resources.getString(R.string.see_more)
-                    holder.noticeMessage.getViewTreeObserver().removeOnGlobalLayoutListener(this)
+                    holder.noticeMessage.viewTreeObserver.removeOnGlobalLayoutListener(this)
                 }
             }
         })
 
 
         holder.seeMore.setOnClickListener {
-            if (!expandable){
+            if (!expandable) {
                 expandable = true
                 holder.noticeMessage.maxLines = Integer.MAX_VALUE
                 holder.seeMore.visibility = View.VISIBLE
                 holder.seeMore.text = holder.itemView.resources.getString(R.string.see_less)
-            }else{
+            } else {
                 expandable = false
                 holder.noticeMessage.maxLines = 3
                 holder.seeMore.text = holder.itemView.resources.getString(R.string.see_more)
@@ -88,20 +89,11 @@ class NoticeListAdapter (val notices: List<Notice>) : RecyclerView.Adapter<Notic
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        internal val tvDesignation: TextView
-        internal val userName: TextView
-        internal val noticeDate: TextView
-        internal val noticeMessage: TextView
-        internal val seeMore: TextView
-        internal val imageUser: ImageView
-
-        init {
-            tvDesignation = itemView.findViewById(R.id.tvDesignation)
-            userName = itemView.findViewById(R.id.userName)
-            imageUser = itemView.findViewById(R.id.imageUser)
-            noticeDate = itemView.findViewById(R.id.noticeDate)
-            noticeMessage = itemView.findViewById(R.id.tvNotice)
-            seeMore = itemView.findViewById(R.id.tvSeeMore)
-        }
+        internal val tvDesignation: TextView = itemView.findViewById(R.id.tvDesignation)
+        internal val userName: TextView = itemView.findViewById(R.id.userName)
+        internal val noticeDate: TextView = itemView.findViewById(R.id.noticeDate)
+        internal val noticeMessage: TextView = itemView.findViewById(R.id.tvNotice)
+        internal val seeMore: TextView = itemView.findViewById(R.id.tvSeeMore)
+        internal val imageUser: ImageView = itemView.findViewById(R.id.imageUser)
     }
 }
