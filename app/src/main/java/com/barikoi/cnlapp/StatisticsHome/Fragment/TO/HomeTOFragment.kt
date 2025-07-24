5c77c56c500f34661b2468e0bd03e3ac88926b8c
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:24:56 +0600

    Organize imports in `HomeTOFragment.kt`
    The import statement for `ProductStockUpdateActivity` was moved to maintain alphabetical order.

--- app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/TO/HomeTOFragment.kt
+++ app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/TO/HomeTOFragment.kt
@@ -20,12 +20,12 @@ import com.android.volley.RequestQueue
 import com.android.volley.VolleyError
 import com.barikoi.cnlapp.Adapter.ViewPagerAdapter
 import com.barikoi.cnlapp.OrderSummary.TO.OrderSummaryTOActivity
-import com.barikoi.cnlapp.ui.ProductStock.ProductStockUpdateActivity
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.StatisticsHome.Activity.ActiveInactiveActivity
 import com.barikoi.cnlapp.StatisticsHome.Adapter.TargetAdapter
 import com.barikoi.cnlapp.StatisticsHome.Model.TargetValue
 import com.barikoi.cnlapp.databinding.FragmentHomeTOBinding
+import com.barikoi.cnlapp.ui.ProductStock.ProductStockUpdateActivity
 import com.barikoi.cnlapp.utils.Api
 import com.barikoi.cnlapp.utils.ApiService.ApiServiceListener
 import com.barikoi.cnlapp.utils.ApiService.ApiServices
