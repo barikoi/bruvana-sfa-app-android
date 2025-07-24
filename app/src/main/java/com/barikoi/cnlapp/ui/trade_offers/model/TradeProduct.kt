79bc6f847285eddff52f57198c30dbc67c377f24
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:49:09 +0600

    Add TradeProduct data class
    This commit introduces a new data class `TradeProduct` with the following properties:
    - `startDate`: String
    - `endDate`: String
    - `discountedPrice`: String
    
    The class implements `Serializable`.

---
+++ app/src/main/java/com/barikoi/cnlapp/ui/trade_offers/model/TradeProduct.kt
@@ -0,0 +1,9 @@
+package com.barikoi.cnlapp.ui.trade_offers.model
+
+import java.io.Serializable
+
+class TradeProduct(
+    var startDate: String,
+    var endDate: String,
+    var discountedPrice: String,
+):Serializable
