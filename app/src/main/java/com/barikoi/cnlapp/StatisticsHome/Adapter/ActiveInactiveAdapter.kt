7c8aeeb860d4e82cd79836ef644c13ec87db9483
Miraz <mmrrahman508@gmail.com>
2025-07-24 11:01:44 +0600

    Refactor ActiveInactiveAdapter to use Coil for image loading
    Replace Glide with Coil for image loading in `ActiveInactiveAdapter`.
    Also, simplify view initialization in `ViewHolder` and improve null/empty checks for text fields.

--- app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Adapter/ActiveInactiveAdapter.kt
+++ app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Adapter/ActiveInactiveAdapter.kt
@@ -6,59 +6,61 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil3.load
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.StatisticsHome.Model.ActiveInactiveSO
-import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ActiveInactiveAdapter(val histories: List<ActiveInactiveSO>) : RecyclerView.Adapter<ActiveInactiveAdapter.ViewHolder>() {
+class ActiveInactiveAdapter(val histories: List<ActiveInactiveSO>) :
+    RecyclerView.Adapter<ActiveInactiveAdapter.ViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ActiveInactiveAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.single_active_inactive_view, parent, false)
+    ): ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.single_active_inactive_view, parent, false)
         return ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: ActiveInactiveAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val oldDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
         val _sdfWatchMonth = SimpleDateFormat("LLL", Locale.ENGLISH)
         val _sdfWatchDate = SimpleDateFormat("dd", Locale.ENGLISH)
         val _sdfWatchtime = SimpleDateFormat("hh:mm a", Locale.ENGLISH)
 
         val mItem = histories[position]
-        if (!mItem.userName.equals("null")){
-            holder.userName.setText(mItem.userName)
-        }else{
+        if (mItem.userName != "null") {
+            holder.userName.text = mItem.userName
+        } else {
             holder.userName.visibility = View.GONE
         }
 
-        if (!mItem.lastUpdatedTime.isNullOrEmpty() && !mItem.lastUpdatedTime.equals("null")){
-            holder.textViewMonth.setText(_sdfWatchMonth.format(oldDate.parse(mItem.lastUpdatedTime)))
-            holder.textViewDate.setText(_sdfWatchDate.format(oldDate.parse(mItem.lastUpdatedTime)))
-            holder.updatedTime.setText(_sdfWatchtime.format(oldDate.parse(mItem.lastUpdatedTime)))
-        }else{
-            holder.updatedTime.setText("--:--")
+        if (mItem.lastUpdatedTime.isNotEmpty() && mItem.lastUpdatedTime != "null") {
+            holder.textViewMonth.text = _sdfWatchMonth.format(oldDate.parse(mItem.lastUpdatedTime))
+            holder.textViewDate.text = _sdfWatchDate.format(oldDate.parse(mItem.lastUpdatedTime))
+            holder.updatedTime.text = _sdfWatchtime.format(oldDate.parse(mItem.lastUpdatedTime))
+        } else {
+            holder.updatedTime.text = "--:--"
         }
-        if (!mItem.imageLink.isNullOrEmpty() && !mItem.imageLink.equals("null")){
-            Glide.with(holder.itemView.context)
-                .load(mItem.imageLink)
-                .into(holder.imageUser)
-        }else{
+        if (mItem.imageLink.isNotEmpty() && mItem.imageLink != "null") {
+            holder.imageUser.load(
+                mItem.imageLink
+            )
+        } else {
             holder.imageUser.visibility = View.GONE
         }
 
-        if (mItem.status.equals("active", true)){
+        if (mItem.status.equals("active", true)) {
             holder.imageStatus.setImageDrawable(holder.itemView.resources.getDrawable(R.drawable.ic_active))
-        }else{
+        } else {
             holder.imageStatus.setImageDrawable(holder.itemView.resources.getDrawable(R.drawable.ic_inactive))
         }
 
-        if (!mItem.checkInAddress.isNullOrEmpty() && !mItem.checkInAddress.equals("null")){
-            holder.inAddress.setText(mItem.checkInAddress)
-        }else{
-            holder.inAddress.setText("")
+        if (mItem.checkInAddress.isNotEmpty() && mItem.checkInAddress != "null") {
+            holder.inAddress.text = mItem.checkInAddress
+        } else {
+            holder.inAddress.text = ""
         }
     }
 
@@ -67,21 +69,12 @@ class ActiveInactiveAdapter(val histories: List<ActiveInactiveSO>) : RecyclerVie
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        internal val textViewMonth: TextView
-        internal val textViewDate: TextView
-        internal val userName: TextView
-        internal val updatedTime: TextView
-        internal val inAddress: TextView
-        internal val imageUser: ImageView
-        internal val imageStatus: ImageView
-        init {
-            textViewMonth = itemView.findViewById(R.id.tvMonth)
-            textViewDate = itemView.findViewById(R.id.tvDate)
-            userName = itemView.findViewById(R.id.soName)
-            updatedTime = itemView.findViewById(R.id.updatedTime)
-            inAddress = itemView.findViewById(R.id.lastAddress)
-            imageUser = itemView.findViewById(R.id.imageUser)
-            imageStatus = itemView.findViewById(R.id.imageStatus)
-        }
+        internal val textViewMonth: TextView = itemView.findViewById(R.id.tvMonth)
+        internal val textViewDate: TextView = itemView.findViewById(R.id.tvDate)
+        internal val userName: TextView = itemView.findViewById(R.id.soName)
+        internal val updatedTime: TextView = itemView.findViewById(R.id.updatedTime)
+        internal val inAddress: TextView = itemView.findViewById(R.id.lastAddress)
+        internal val imageUser: ImageView = itemView.findViewById(R.id.imageUser)
+        internal val imageStatus: ImageView = itemView.findViewById(R.id.imageStatus)
     }
 }
