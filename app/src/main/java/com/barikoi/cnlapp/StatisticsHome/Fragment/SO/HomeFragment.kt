854f12a02c38315465f1e69e11badf59a7495dd0
Miraz <mmrrahman508@gmail.com>
2025-07-27 14:47:05 +0600

    Refactor: Move ViewPagerAdapter to ui.adapter package
    The `ViewPagerAdapter` class has been moved from `com.barikoi.cnlapp.Adapter` to `com.barikoi.cnlapp.ui.adapter`.
    
    This change affects the following files:
    - `app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/TO/HomeTOFragment.kt`
    - `app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/SO/HomeFragment.kt`
    
    The import statements in these files have been updated to reflect the new location of `ViewPagerAdapter`.

--- app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/SO/HomeFragment.kt
+++ app/src/main/java/com/barikoi/cnlapp/StatisticsHome/Fragment/SO/HomeFragment.kt
@@ -19,11 +19,11 @@ import androidx.viewpager2.widget.ViewPager2
 import com.android.volley.NetworkResponse
 import com.android.volley.RequestQueue
 import com.android.volley.VolleyError
-import com.barikoi.cnlapp.Adapter.ViewPagerAdapter
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.StatisticsHome.Adapter.TargetAdapter
 import com.barikoi.cnlapp.StatisticsHome.Model.TargetValue
 import com.barikoi.cnlapp.databinding.FragmentHomeBinding
+import com.barikoi.cnlapp.ui.adapter.ViewPagerAdapter
 import com.barikoi.cnlapp.ui.main.MainActivity.Companion.routeName_selected
 import com.barikoi.cnlapp.utils.Api
 import com.barikoi.cnlapp.utils.ApiService.ApiServiceListener
