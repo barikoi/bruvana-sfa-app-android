446e22256f2070ac620fe17956a920b364633407
Miraz <mmrrahman508@gmail.com>
2025-07-28 17:17:17 +0600

    Refactor ConfirmOrderProductListAdapter to use property access syntax
    This commit refactors `ConfirmOrderProductListAdapter` to use property access syntax instead of field access for `Products` model properties.
    
    Additionally:
    - The "Offer" `TextView` is now made visible only if `offerID` is not null or empty.
    - Subtotal calculation is adjusted for products with offers, multiplying `orderedTotalPrice` by `orderedQuantity`.
    - Number formatting is now handled by the `format()` extension function.

--- app/src/main/java/com/barikoi/cnlapp/order_create/Adapter/ConfirmOrderProductListAdapter.kt
+++ app/src/main/java/com/barikoi/cnlapp/order_create/Adapter/ConfirmOrderProductListAdapter.kt
@@ -4,12 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.barikoi.cnlapp.Model.Products
 import com.barikoi.cnlapp.R
-import java.text.DecimalFormat
+import com.barikoi.cnlapp.utils.extension.format
 
-class ConfirmOrderProductListAdapter(var mValues: List<Products>):RecyclerView.Adapter<ConfirmOrderProductListAdapter.ViewHolder>() {
+class ConfirmOrderProductListAdapter(mValues: List<Products>) :
+    RecyclerView.Adapter<ConfirmOrderProductListAdapter.ViewHolder>() {
 
     var productList: List<Products> = mValues
 
@@ -17,16 +19,24 @@ class ConfirmOrderProductListAdapter(var mValues: List<Products>):RecyclerView.A
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.single_confirm_order_product_listview, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.single_confirm_order_product_listview, parent, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var dformat = DecimalFormat("#.##")
-        holder.productName.text = productList[position].product_name
-        holder.addedProduct.text = productList[position].ordered_quantity.toString()
-        holder.tvSubtoal.text = dformat.format(productList[position].ordered_total_price).toString()
-        holder.tvUnitName.text = productList[position].unit_name
+        holder.productName.text = productList[position].productName
+        holder.tvOffer.isVisible = !productList[position].offerID.isNullOrEmpty()
+        holder.addedProduct.text = productList[position].orderedQuantity.toString()
+        holder.tvSubTotal.text =
+            if (productList[position].offerID.isNullOrEmpty()) {
+                productList[position].orderedTotalPrice.toString().format()
+            } else {
+                (productList[position].orderedTotalPrice * productList[position].orderedQuantity).toString()
+                    .format()
+            }
+
+        holder.tvUnitName.text = productList[position].unitName
     }
 
     override fun getItemCount(): Int {
@@ -34,16 +44,10 @@ class ConfirmOrderProductListAdapter(var mValues: List<Products>):RecyclerView.A
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        internal val productName: TextView
-        internal val addedProduct: TextView
-        internal val tvSubtoal : TextView
-        internal val tvUnitName : TextView
-
-        init {
-            productName = itemView.findViewById(R.id.tvProductName)
-            addedProduct = itemView.findViewById(R.id.addedProductCount)
-            tvSubtoal = itemView.findViewById(R.id.tvTotalPrice)
-            tvUnitName = itemView.findViewById(R.id.tvUnitName)
-        }
+        internal val productName: TextView = itemView.findViewById(R.id.tvProductName)
+        internal val addedProduct: TextView = itemView.findViewById(R.id.addedProductCount)
+        internal val tvSubTotal: TextView = itemView.findViewById(R.id.tvTotalPrice)
+        internal val tvUnitName: TextView = itemView.findViewById(R.id.tvUnitName)
+        internal val tvOffer: TextView = itemView.findViewById(R.id.tvOffer)
     }
 }
