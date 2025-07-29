0dc7167a43848732920088fc5a37cbf6a7a35dd1
Miraz <mmrrahman508@gmail.com>
2025-07-29 17:07:06 +0600

    Remove setting Calendar to first day of month
    The `cal.set(Calendar.DAY_OF_MONTH, 1)` line was removed in `SummaryDetailsActivity.kt`. This means that `formattedStartDate` will now default to the current date instead of the first day of the current month.

--- app/src/main/java/com/barikoi/cnlapp/ui/summary_details/SummaryDetailsActivity.kt
+++ app/src/main/java/com/barikoi/cnlapp/ui/summary_details/SummaryDetailsActivity.kt
@@ -75,7 +75,6 @@ class SummaryDetailsActivity : BaseActivity() {
         }
 
         val cal = Calendar.getInstance()
-        cal.set(Calendar.DAY_OF_MONTH, 1)
 
         formattedStartDate = cal.time.formatDateWithLocale()
         formattedEndDate = Date().formatDateWithLocale()
