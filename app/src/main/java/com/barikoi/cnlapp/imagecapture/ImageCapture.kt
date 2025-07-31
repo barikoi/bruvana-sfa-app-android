45df96a0b7aa47348c91f8bc5301e24356e362c4
Miraz <mmrrahman508@gmail.com>
2025-07-31 12:50:32 +0600

    Add safety check to `removeImages` and clarify permission dialog
    This commit introduces two changes to the `ImageCapture.kt` file:
    
    1.  A null and empty check has been added to the `removeImages` function. This prevents potential issues if the `imageRecyclerAdapter` is null or has no items when the function is called.
    2.  The permission request dialog message for accessing gallery photos has been updated to be more specific: "Barikoi needs external storage read permission to access gallery photos".

--- app/src/main/java/com/barikoi/cnlapp/imagecapture/ImageCapture.kt
+++ app/src/main/java/com/barikoi/cnlapp/imagecapture/ImageCapture.kt
@@ -209,6 +209,10 @@ class ImageCapture(context: Context?, attrs: AttributeSet?) :
     }
 
     fun removeImages() {
+        if (imageRecyclerAdapter == null || imageRecyclerAdapter!!.itemCount == 0) {
+            return
+        }
+
         for (i in 0 until imageRecyclerAdapter!!.itemCount) {
             imageRecyclerAdapter!!.removeAt(0)
         }
@@ -523,14 +527,16 @@ class ImageCapture(context: Context?, attrs: AttributeSet?) :
                 // sees the explanation, try again to request the permission.
                 AlertDialog.Builder(activity)
                     .setMessage("Barikoi needs external storage read permission to access gallery photos")
-                    .setPositiveButton(R.string.ok,
+                    .setPositiveButton(
+                        R.string.ok,
                         DialogInterface.OnClickListener { dialog, which ->
                             ActivityCompat.requestPermissions(
                                 activity!!, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                                 MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
                             )
                         })
-                    .setNegativeButton(R.string.cancel,
+                    .setNegativeButton(
+                        R.string.cancel,
                         DialogInterface.OnClickListener { dialog, which -> })
                     .create()
                     .show()
