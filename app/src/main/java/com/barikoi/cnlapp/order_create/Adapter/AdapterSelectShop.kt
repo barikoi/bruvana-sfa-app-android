723137db294e5e299bf45fb282da82bf99275317
Miraz <mmrrahman508@gmail.com>
2025-07-31 12:46:09 +0600

    Display Outlet ID, Type, and Last Order Date in AdapterSelectShop
    This commit updates the `AdapterSelectShop` to display additional information for each shop item:
    
    - Outlet ID is now shown.
    - Outlet Type is now shown.
    - The last order date is displayed if available, formatted using the `formateDate` extension.

--- app/src/main/java/com/barikoi/cnlapp/order_create/Adapter/AdapterSelectShop.kt
+++ app/src/main/java/com/barikoi/cnlapp/order_create/Adapter/AdapterSelectShop.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.barikoi.cnlapp.R
 import com.barikoi.cnlapp.data.remote.models.Outlet
 import com.barikoi.cnlapp.databinding.ItemSelectShopBinding
+import com.barikoi.cnlapp.utils.extension.formateDate
 import java.util.Locale
 
 class AdapterSelectShop(
@@ -50,6 +51,18 @@ class AdapterSelectShop(
 
         holder.binding.btnDetails.setOnClickListener {
             onSelectShopListener(filterShopList[position])
+        }
+
+        holder.binding.tvOutletId.text = filterShopList[position].id.toString()
+        holder.binding.tvOutletType.text = filterShopList[position].outletType
+
+
+        if (!filterShopList[position].lastOrderedAt.isNullOrEmpty()) {
+            holder.binding.orderDate.text =
+                holder.itemView.context.resources.getString(
+                    R.string.last_order_date,
+                    shopList[position].lastOrderedAt!!.formateDate()
+                )
         }
     }
 
