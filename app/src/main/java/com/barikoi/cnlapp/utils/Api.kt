9ecd2d1e0cbc2983ebb173116baa524a2d16949e
Miraz <mmrrahman508@gmail.com>
2025-09-16 15:21:24 +0600

    Add: New API endpoint for DB house-wise SO list
    A new API endpoint `get_all_so_list_db_wise` has been added to `Api.kt`. This endpoint is designed to fetch a list of Sales Officers (SOs) based on their distribution house.

--- app/src/main/java/com/barikoi/cnlapp/utils/Api.kt
+++ app/src/main/java/com/barikoi/cnlapp/utils/Api.kt
@@ -83,6 +83,7 @@ object Api {
 
     /*TO Part*/
     val get_all_so_list = base_url+"api/v1/get-so"
+    val get_all_so_list_db_wise = base_url+"api/v1/db-house-wise-so"
     val get_all_to_list = base_url+"api/v1/get-to"
     val get_dh_list = base_url+"api/v1/db-houses"
 
