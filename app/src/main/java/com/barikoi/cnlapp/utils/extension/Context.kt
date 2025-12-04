8430a695091fcbd76f9194e374913dc3d9a0e6c8
Miraz <mmrrahman508@gmail.com>
2025-12-04 14:34:30 +0600

    Add: Implement compatibility methods for Parcelable extras in Intent

--- app/src/main/java/com/barikoi/cnlapp/utils/extension/Context.kt
+++ app/src/main/java/com/barikoi/cnlapp/utils/extension/Context.kt
@@ -1,5 +1,6 @@
 package com.barikoi.cnlapp.utils.extension
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
@@ -10,5 +11,23 @@ inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? 
     } else {
         @Suppress("DEPRECATION")
         getParcelable(key)
+    }
+}
+
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key)
+    }
+}
+
+inline fun <reified T : Parcelable> Intent.getParcelableArrayListExtraCompat(key: String): ArrayList<T>? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableArrayListExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableArrayListExtra(key)
     }
 }
